f34876cb5461dfb2710fd078d71b7d42ec28dd49
Taruna Wahyudi <wahyuditaruna97@gmail.com>
2021-05-06 23:36:43 +0700

    Automated Testing - Show Resto Scenario Search Specs

--- specs/favoriteRestoSearchSpec.js
+++ specs/favoriteRestoSearchSpec.js
@@ -3,7 +3,15 @@ import FavoriteRestoIdb from '../src/scripts/data/favoritresto-idb';
 
 describe('Searching restos', () => {
 
-  beforeEach(() => {
+  let presenter;
+
+  const searchRestos = (query) => {
+    const queryElement = document.getElementById('query');
+    queryElement.value = query;
+    queryElement.dispatchEvent(new Event('change'));
+  };
+
+  const setRestoSearchContainer = () => {
     document.body.innerHTML = `
         <div id="resto-search-container">
             <input id="query" type="text">
@@ -13,28 +21,65 @@ describe('Searching restos', () => {
             </div>
         </div>
         `;
+  }
+
+  const constructPresenter = () => {
+    spyOn(FavoriteRestoIdb, 'searchRestos');
+    presenter = new FavoriteRestoSearchPresenter({ 
+      favoriteRestos: FavoriteRestoIdb 
+    });
+  }
+
+  beforeEach(() => {
+    setRestoSearchContainer();
+    constructPresenter();
   });
 
   it('should be able to capture the query typed by the user', () => {
-    spyOn(FavoriteRestoIdb, 'searchRestos');
-    const presenter = new FavoriteRestoSearchPresenter({ favoriteRestos: FavoriteRestoIdb });
-
-    const queryElement = document.getElementById('query');
-    queryElement.value = 'resto a';
-    queryElement.dispatchEvent(new Event('change'));
+    searchRestos('resto a');
 
     expect(presenter.latestQuery).toEqual('resto a');
   });
 
   it('should ask the model to search for liked restos', () => {
-    spyOn(FavoriteRestoIdb, 'searchRestos');
-    const presenter = new FavoriteRestoSearchPresenter({ favoriteRestos: FavoriteRestoIdb });
-
-    const queryElement = document.getElementById('query');
-    queryElement.value = 'resto a';
-    queryElement.dispatchEvent(new Event('change'));
+    searchRestos('resto a');
 
     expect(FavoriteRestoIdb.searchRestos)
       .toHaveBeenCalledWith('resto a');
+  });
+
+  it('should show the found restos', () => {
+    presenter._showFoundRestos([{ id: 1 }]);
+    expect(document.querySelectorAll('.resto').length).toEqual(1);
+
+    presenter._showFoundRestos(
+      [{ id: 1, name: 'Satu' }, { id: 2, name: 'Dua' }]
+    );
+
+    expect(document.querySelectorAll('.resto').length).toEqual(2);
+  });
+
+  it('should show the name of the found restos', () => {
+    presenter._showFoundRestos([{
+      id: 1,
+      name: 'Satu',
+    }]);
+    expect(document.querySelectorAll('.resto__name').item(0).textContent)
+      .toEqual('Satu');
+    
+    presenter._showFoundRestos(
+      [{ id: 1, name: 'Satu' }, { id: 2, name: 'Dua'}],
+    );
+
+    const restoNames = document.querySelectorAll('.resto__name');
+    expect(restoNames.item(0).textContent).toEqual('Satu');
+    expect(restoNames.item(1).textContent).toEqual('Dua');
+  })
+
+  it('should show - for found resto without name', () => {
+    presenter._showFoundRestos([{ id: 1 }]);
+
+    expect(document.querySelectorAll('.resto__name').item(0).textContent)
+      .toEqual('-');
   })
 });
--- src/scripts/views/pages/liked-restos/favorite-resto-search-presenter.js
+++ src/scripts/views/pages/liked-restos/favorite-resto-search-presenter.js
@@ -7,9 +7,22 @@ class FavoriteRestoSearchPresenter {
   _listenToSearchRequestByUser() {
     this._queryElement = document.getElementById('query');
     this._queryElement.addEventListener('change', (event) => {
-      this._latestQuery = event.target.value;
-      this._favoriteRestos.searchRestos(this._latestQuery);
+      this._searchRestos(event.target.value);
     });
+  }
+
+  _searchRestos(latestQuery) {
+    this._latestQuery = latestQuery;
+    this._favoriteRestos.searchRestos(this.latestQuery);
+  }
+
+  _showFoundRestos(restos) {
+    const html = restos.reduce(
+      (carry, resto) => carry.concat(`<li class="resto"><span class="resto__name">${resto.name || '-'}</span></li>`),
+      '',
+    );
+
+    document.querySelector('.restos').innerHTML = html;
   }
  
   get latestQuery() {
